84091dbb6868009fb79017723918672edbda7920
Salman <salman26080@gmail.com>
2024-12-23 08:00:14 +0800

    trying different deployment structure

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SalmanAlfarisi5.github.io/",
+  base: "htpps://SalmanAlfarisi5.github.io",
   plugins: [react()],
   css: {
     modules: {
